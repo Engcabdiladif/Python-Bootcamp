8bc35bb4d0849a35472d1b70c2b0affcb2a8368f
cabdiladiif <abdiladiifsaalax4@gmail.com>
2022-03-10 12:04:08 +0300

    this lesson 3

---
+++ Operators.py
@@ -0,0 +1,90 @@
+ # Date  09/03/2022    
+
+# operators        
+# this language have like *,+,\, this calle arimetic
+# modulus or remenders  %
+#
+# x= 5
+# y= 2
+# print(x % y)
+
+# Exponentiation  jibaarid weye  **
+
+# x = 5
+# y = 3
+# print(x ** y)
+
+# floor  wuun hadhaag ka saaraya meehs as int uu isku soo qaoraya
+
+# x = 15
+# y = 2
+# print(x // 2)
+
+# comparison   is barbar dhig
+
+#  is equl to : ==
+# isn equl to: !=
+# great then: >
+# less then: <
+# gret or equl: >=
+# less or erual: <=
+                       # Example
+
+# x = 10
+# y = 9
+# print( x==y)  # is false
+#
+#                                   this is comparsison
+# T = 34
+# z = 35
+# print( T <= z) # is True
+#
+# p = 56
+# u = 76
+# print(p >= u) # is false
+
+
+      # Logical operator
+
+# and ,or ,not   kuligood ture iyo false soo sarayan
+
+# y = 5
+# x = 5
+# t = 10
+# j = 18
+# #
+# #
+# if not(y ==x and t == j):
+#     print('they are all same ')
+# else:
+#     print('not all same')
+
+  # identity operators  is , is not   ture or false
+
+# x =  5
+# y = 5
+# print(x is y)
+                         # this called identity operators
+# m =10
+# k = 12
+# print(m is not k )
+
+
+# membership   using in , not in    trure and false
+
+#
+# x = ["canbo","saytuun","kaluun","xabxab","canbo laf"]
+# 
+
+
+
+
+# y = "canbo"
+# print(y in x)
+#
+#
+# c = ["kaluun","digaag"]
+#
+# h = "moos"
+#
+# print(y not in c)
