5591d0730e78e131f23d5f49b813944b6b626ab4
cabdiladiif <abdiladiifsaalax4@gmail.com>
2022-03-15 18:35:40 +0300

    yes

---
+++ While loop.py
@@ -0,0 +1,39 @@
+# while loop  = waysoconaysaa ilaa intay false ka noqonay
+# markadan ogayn data masheey ku dhmaynayso
+# 
+# i = 0
+# while i <= 10:
+#     print(i)       # this is simply example
+#     i += 1
+# 
+# 
+# Break and continue
+# 
+# i = 1
+# while i < 10:
+#     print(i)            # inadaa samayso Break waxa ka cawinaysa
+#     if i== 4:           # inaad isticmaasho conditional if statement
+#         print("stop bro")
+#         break
+#         i += 1
+# 
+# continue
+# i = 1
+# while i < 10:
+#         print(i)
+#         i += 1
+#         if i == 4:
+#          print("found")
+#         continue
+# 
+# 
+# 
+# Else
+# 
+# i = 1
+# while i < 5:
+#     print(i, "is less then 5")
+#     i += 1
+# else:
+#     print(i,"is not less than 5")
+# 
